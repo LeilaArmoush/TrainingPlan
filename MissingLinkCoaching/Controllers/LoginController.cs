eddd23a9762e89abb9061abadfd52f24a159cb0f
Leila Armoush <leila.armoush.1990@gmail.com>
2018-06-28 22:51:17 +0100

    Add project files.

---
+++ MissingLinkCoaching/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using MissingLinkCoaching.Models;
+using System.Web.Security;
+
+namespace MissingLinkCoaching.Controllers
+{
+    public class MemberController : SurfaceController
+    {
+        public ActionResult RenderLogin()
+        {
+            return PartialView("_Login", new LoginModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Name, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, false);
+                    UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+                    if (myHelper.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return Redirect("/login/");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView("_Logout", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return Redirect("/login/");
+        }
+    }
+}
---
+++ MissingLinkCoaching/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Services;
+
+namespace MissingLinkCoaching.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Username")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Passsword")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
