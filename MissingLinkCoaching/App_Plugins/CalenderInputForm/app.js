9bc38ded70343888914c6374efcc5f1ace65b54e
Leila Armoush <leila.armoush.1990@gmail.com>
2018-07-11 21:40:25 +0100

    updated javascript

--- MissingLinkCoaching/App_Plugins/CalenderInputForm/app.js
+++ MissingLinkCoaching/App_Plugins/CalenderInputForm/app.js
@@ -1,1 +1,8 @@
-﻿alert("test");
+﻿angular.module('umbraco').controller('calenderInputFormController', function ($scope) {
+
+    console.log("hi there");
+    $scope.expandCell = function () {
+        console.log("ouch");
+
+    }
+});
